7aebad909a4e4a20a2aedec58cdf4317fe84365c
JavierGarridoPousibet <jgp0061@alu.medac.es>
2025-10-16 18:47:28 +0200

    Update tema 4 v2

--- Tema 4/ejemplosPHP/comparador.php
+++ Tema 4/ejemplosPHP/comparador.php
@@ -1,4 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
     $n1=intval($_GET['valor1']);
     $n2=intval($_GET['valor2']);
     if ($n1>$n2){
@@ -7,4 +15,11 @@
     else{
     echo "El número mayor es (".$n2.")";
     }
-?>
+    ?>
+</body>
+</html>
+
+
+
+
+
